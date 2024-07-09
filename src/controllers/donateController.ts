197f70deb5545e2a67ce42fb5ab2f0cd765b4c21
vididvidid <vididvidid@gmail.com>
2024-07-10 00:12:28 +0530

    fix issue

--- src/controllers/donateController.ts
+++ src/controllers/donateController.ts
@@ -145,7 +145,7 @@ export const donateRedirect = async (req: Request, res: Response) => {
             try {
                 const response = await axios.request(options);
                 if (response.data.success === true) {
-                    donate.status = response.data.data.code;
+                    donate.status = response.data.code;
                     donate.paymentId = response.data.data.transactionId || 0;
                     await donate.save();
                     console.log(response.data);
